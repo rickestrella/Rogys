79e2d831756f052a2d0986e1f9e8c9fa617afe2d
Ricardo Estrella <rjestrellas95@gmail.com>
2021-03-19 03:16:09 -0500

    Added Firebase

--- app/src/main/java/com/techpig/rogys/SplashActivity.kt
+++ app/src/main/java/com/techpig/rogys/SplashActivity.kt
@@ -1,11 +1,43 @@
 package com.techpig.rogys
 
+import android.content.Intent
+import android.graphics.Typeface
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.Window
+import android.view.WindowInsets
+import android.view.WindowManager
+import android.widget.TextView
 
 class SplashActivity : AppCompatActivity() {
+
+    lateinit var tvAppName: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        tvAppName = findViewById(R.id.tvAppName)
+
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+
+        @Suppress("DEPRECATION")
+        Handler().postDelayed({
+            startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+            finish()
+        }, 2500)
+
+        val montserratRegular :Typeface = Typeface.createFromAsset(assets, "Montserrat-Regular.ttf")
+        tvAppName.typeface = montserratRegular
     }
 }
